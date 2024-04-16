84dc45764b6dbf0d97835205d986b116269a04d4
skajihara <t15cs015@gmail.com>
2024-04-16 18:38:15 +0900

    コメント削除

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,29 +1,11 @@
-<script>
-// export default {
-//   props: {
-//     userId: {
-//       type: String,
-//       required: true
-//     },
-//     userName: {
-//       type: String,
-//       required: true
-//     },
-//     userBio: {
-//       type: String,
-//       required: false,
-//       default: 'default text'
-//     }
-//   }
-// }
-</script>
+<script></script>
 
 <template>
   <div>
     <h1>プロフィール</h1>
-    <p>ユーザーID: {{ userId }}</p>
-    <p>ユーザー名: {{ userName }}</p>
-    <p>自己紹介: {{ userBio }}</p>
+    <!-- <p>ユーザーID: {{ $route.props.userId }}</p>
+    <p>ユーザー名: {{ $route.props.userName }}</p>
+    <p>自己紹介: {{ $route.props.userBio }}</p> -->
     <router-link :to="{ name: 'timeline' }">タイムラインへ</router-link>
     <p>{{ $route }}</p>
   </div>
