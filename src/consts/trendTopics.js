0595c04f61fa130eb1293e9e7afc576f6d612a27
skajihara <t15cs015@gmail.com>
2024-04-23 21:20:13 +0900

    トピックファイル追加

---
+++ src/consts/trendTopics.js
@@ -0,0 +1,553 @@
+import { ref } from 'vue'
+export const trendTopics = ref([
+  {
+    genre: '日本',
+    word: '五輪',
+    num: 4289
+  },
+  {
+    genre: '日本',
+    word: 'コロナ',
+    num: 7531
+  },
+  {
+    genre: '日本',
+    word: '桜',
+    num: 2189
+  },
+  {
+    genre: '日本',
+    word: '緊急事態宣言',
+    num: 3647
+  },
+  {
+    genre: '日本',
+    word: '新型コロナ',
+    num: 5912
+  },
+  {
+    genre: '日本',
+    word: 'アベノマスク',
+    num: 1056
+  },
+  {
+    genre: '日本',
+    word: 'デジタル庁',
+    num: 8364
+  },
+  {
+    genre: '日本',
+    word: '電子マネー',
+    num: 6543
+  },
+  {
+    genre: '日本',
+    word: 'ワクチン',
+    num: 3987
+  },
+  {
+    genre: '日本',
+    word: 'ロックダウン',
+    num: 4231
+  },
+  {
+    genre: 'ニュース',
+    word: '選挙',
+    num: 5897
+  },
+  {
+    genre: 'ニュース',
+    word: '政権交代',
+    num: 2643
+  },
+  {
+    genre: 'ニュース',
+    word: '内閣改造',
+    num: 4219
+  },
+  {
+    genre: 'ニュース',
+    word: '財務省',
+    num: 3756
+  },
+  {
+    genre: 'ニュース',
+    word: '国会',
+    num: 6834
+  },
+  {
+    genre: 'ニュース',
+    word: '衆院選',
+    num: 5971
+  },
+  {
+    genre: 'ニュース',
+    word: '参院選',
+    num: 4023
+  },
+  {
+    genre: 'ニュース',
+    word: '民主党',
+    num: 3145
+  },
+  {
+    genre: 'ニュース',
+    word: '自民党',
+    num: 5387
+  },
+  {
+    genre: 'ニュース',
+    word: '野党',
+    num: 4861
+  },
+  {
+    genre: '音楽',
+    word: '新曲',
+    num: 7854
+  },
+  {
+    genre: '音楽',
+    word: 'アーティスト',
+    num: 3692
+  },
+  {
+    genre: '音楽',
+    word: 'コンサート',
+    num: 4823
+  },
+  {
+    genre: '音楽',
+    word: 'ライブ',
+    num: 5978
+  },
+  {
+    genre: '音楽',
+    word: '音楽番組',
+    num: 3567
+  },
+  {
+    genre: '音楽',
+    word: '音楽フェス',
+    num: 2314
+  },
+  {
+    genre: '音楽',
+    word: 'J-POP',
+    num: 7145
+  },
+  {
+    genre: '音楽',
+    word: 'アイドル',
+    num: 5987
+  },
+  {
+    genre: '音楽',
+    word: 'MV',
+    num: 4021
+  },
+  {
+    genre: '音楽',
+    word: 'ヒット曲',
+    num: 5961
+  },
+  {
+    genre: 'ファッション',
+    word: 'トレンド',
+    num: 4567
+  },
+  {
+    genre: 'ファッション',
+    word: 'ブランド',
+    num: 5423
+  },
+  {
+    genre: 'ファッション',
+    word: 'ファッションショー',
+    num: 3678
+  },
+  {
+    genre: 'ファッション',
+    word: 'コレクション',
+    num: 3215
+  },
+  {
+    genre: 'ファッション',
+    word: 'スタイル',
+    num: 4987
+  },
+  {
+    genre: 'ファッション',
+    word: 'ファッションアイテム',
+    num: 4089
+  },
+  {
+    genre: 'ファッション',
+    word: 'ファッションブランド',
+    num: 5346
+  },
+  {
+    genre: 'ファッション',
+    word: 'デザイナー',
+    num: 6234
+  },
+  {
+    genre: 'ファッション',
+    word: 'スタイリスト',
+    num: 2854
+  },
+  {
+    genre: 'ファッション',
+    word: 'コーディネート',
+    num: 3765
+  },
+  {
+    genre: 'ゲーム',
+    word: '新作',
+    num: 2345
+  },
+  {
+    genre: 'ゲーム',
+    word: '人気ゲーム',
+    num: 5821
+  },
+  {
+    genre: 'ゲーム',
+    word: 'オンラインゲーム',
+    num: 4769
+  },
+  {
+    genre: 'ゲーム',
+    word: 'eスポーツ',
+    num: 3412
+  },
+  {
+    genre: 'ゲーム',
+    word: 'ゲーム大会',
+    num: 5187
+  },
+  {
+    genre: 'ゲーム',
+    word: 'プレイ動画',
+    num: 6934
+  },
+  {
+    genre: 'ゲーム',
+    word: 'ゲームイベント',
+    num: 4289
+  },
+  {
+    genre: 'ゲーム',
+    word: 'ゲーム開発',
+    num: 5721
+  },
+  {
+    genre: 'ゲーム',
+    word: '攻略法',
+    num: 3987
+  },
+  {
+    genre: 'ゲーム',
+    word: 'ゲームコンソール',
+    num: 6078
+  },
+  {
+    genre: 'スポーツ',
+    word: 'オリンピック',
+    num: 6789
+  },
+  {
+    genre: 'スポーツ',
+    word: 'サッカー',
+    num: 5487
+  },
+  {
+    genre: 'スポーツ',
+    word: '野球',
+    num: 6254
+  },
+  {
+    genre: 'スポーツ',
+    word: 'バスケットボール',
+    num: 4021
+  },
+  {
+    genre: 'スポーツ',
+    word: 'テニス',
+    num: 5398
+  },
+  {
+    genre: 'スポーツ',
+    word: 'ゴルフ',
+    num: 4785
+  },
+  {
+    genre: 'スポーツ',
+    word: '柔道',
+    num: 3645
+  },
+  {
+    genre: 'スポーツ',
+    word: '相撲',
+    num: 2854
+  },
+  {
+    genre: 'スポーツ',
+    word: '陸上競技',
+    num: 5189
+  },
+  {
+    genre: 'スポーツ',
+    word: 'フィギュアスケート',
+    num: 3978
+  },
+  {
+    genre: '食べ物',
+    word: 'グルメ',
+    num: 8912
+  },
+  {
+    genre: '食べ物',
+    word: 'レシピ',
+    num: 6498
+  },
+  {
+    genre: '食べ物',
+    word: '料理',
+    num: 7234
+  },
+  {
+    genre: '食べ物',
+    word: '食材',
+    num: 5987
+  },
+  {
+    genre: '食べ物',
+    word: 'レストラン',
+    num: 4785
+  },
+  {
+    genre: '食べ物',
+    word: 'カフェ',
+    num: 5832
+  },
+  {
+    genre: '食べ物',
+    word: 'スイーツ',
+    num: 4621
+  },
+  {
+    genre: '食べ物',
+    word: '和食',
+    num: 5289
+  },
+  {
+    genre: '食べ物',
+    word: '洋食',
+    num: 3145
+  },
+  {
+    genre: '食べ物',
+    word: '中華料理',
+    num: 3967
+  },
+  {
+    genre: '政治',
+    word: '改革',
+    num: 1234
+  },
+  {
+    genre: '政治',
+    word: '内閣',
+    num: 5896
+  },
+  {
+    genre: '政治',
+    word: '首相',
+    num: 4167
+  },
+  {
+    genre: '政治',
+    word: '国会',
+    num: 5489
+  },
+  {
+    genre: '政治',
+    word: '政策',
+    num: 4321
+  },
+  {
+    genre: '政治',
+    word: '議員',
+    num: 3754
+  },
+  {
+    genre: '政治',
+    word: '内閣総理大臣',
+    num: 6892
+  },
+  {
+    genre: '政治',
+    word: '閣僚',
+    num: 4563
+  },
+  {
+    genre: '政治',
+    word: '公職選挙法',
+    num: 3125
+  },
+  {
+    genre: '政治',
+    word: '立法',
+    num: 4789
+  },
+  {
+    genre: 'ビジネス',
+    word: '株価',
+    num: 5678
+  },
+  {
+    genre: 'ビジネス',
+    word: '経済',
+    num: 6942
+  },
+  {
+    genre: 'ビジネス',
+    word: '企業',
+    num: 5387
+  },
+  {
+    genre: 'ビジネス',
+    word: '投資',
+    num: 4279
+  },
+  {
+    genre: 'ビジネス',
+    word: '不正',
+    num: 3124
+  },
+  {
+    genre: 'ビジネス',
+    word: '取引',
+    num: 4965
+  },
+  {
+    genre: 'ビジネス',
+    word: 'ビジネスマン',
+    num: 5723
+  },
+  {
+    genre: 'ビジネス',
+    word: '市場',
+    num: 3758
+  },
+  {
+    genre: 'ビジネス',
+    word: '経営',
+    num: 6487
+  },
+  {
+    genre: 'ビジネス',
+    word: '企業倫理',
+    num: 4912
+  },
+  {
+    genre: '旅行',
+    word: '観光地',
+    num: 9012
+  },
+  {
+    genre: '旅行',
+    word: 'ツアー',
+    num: 6743
+  },
+  {
+    genre: '旅行',
+    word: '宿泊',
+    num: 7432
+  },
+  {
+    genre: '旅行',
+    word: '国内旅行',
+    num: 5487
+  },
+  {
+    genre: '旅行',
+    word: '海外旅行',
+    num: 6978
+  },
+  {
+    genre: '旅行',
+    word: '観光スポット',
+    num: 5123
+  },
+  {
+    genre: '旅行',
+    word: '旅行者',
+    num: 6234
+  },
+  {
+    genre: '旅行',
+    word: '観光業',
+    num: 3965
+  },
+  {
+    genre: '旅行',
+    word: 'ホテル',
+    num: 4876
+  },
+  {
+    genre: '旅行',
+    word: 'レジャー',
+    num: 3421
+  },
+  {
+    genre: 'エンターテインメント',
+    word: '映画',
+    num: 7890
+  },
+  {
+    genre: 'エンターテインメント',
+    word: 'ドラマ',
+    num: 5821
+  },
+  {
+    genre: 'エンターテインメント',
+    word: 'テレビ番組',
+    num: 6534
+  },
+  {
+    genre: 'エンターテインメント',
+    word: '俳優',
+    num: 4769
+  },
+  {
+    genre: 'エンターテインメント',
+    word: '女優',
+    num: 3987
+  },
+  {
+    genre: 'エンターテインメント',
+    word: '映画祭',
+    num: 5278
+  },
+  {
+    genre: 'エンターテインメント',
+    word: 'アカデミー賞',
+    num: 3412
+  },
+  {
+    genre: 'エンターテインメント',
+    word: '舞台',
+    num: 4691
+  },
+  {
+    genre: 'エンターテインメント',
+    word: '演劇',
+    num: 5967
+  },
+  {
+    genre: 'エンターテインメント',
+    word: 'ミュージカル',
+    num: 4289
+  }
+])
