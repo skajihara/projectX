e263828f35b45caa96c7a65a81aeb7d35945eaa8
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2024-04-23 15:22:39 +0900

    Installed unplugin-vue-components.
    And I changed vite.config.js for upper module.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
   ],
   resolve: {
     alias: {
