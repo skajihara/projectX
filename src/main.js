4e6c8f43bc83625e9312df75f6c7a54b087b1ca3
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2024-04-23 15:21:21 +0900

    Installed bootstrap-vue-next.
    I just looked below doc.
    - https://bootstrap-vue-next.github.io/bootstrap-vue-next/docs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import './assets/main.css'
 import { createApp } from 'vue'
+import {createBootstrap} from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
 
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 const app = createApp(App)
 app.use(router)
+app.use(createBootstrap()) // Important
 app.mount('#app')
