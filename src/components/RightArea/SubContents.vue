911e35e50e5d0e4c4c25ca3fe9ed08c0b4a3fb0b
skajihara <t15cs015@gmail.com>
2024-04-25 14:26:46 +0900

    サブコンテンツ検索フォーム修正

--- src/components/RightArea/SubContents.vue
+++ src/components/RightArea/SubContents.vue
@@ -1,41 +1,55 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import TrendTopics from './SubContents/TrendTopics.vue'
 import RecommendedUsers from './SubContents/RecommendedUsers.vue'
+import { accounts } from '@/consts/accounts.js'
 
 const searchQuery = ref('')
-const filteredItems = ref([])
-const items = [
-  { id: 1, name: 'abcDEFhijKLMn' },
-  { id: 2, name: '-0oll;ns' },
-  { id: 3, name: 'さんぷるアイテムAa_912' }
-]
-const search = () => {
-  filteredItems.value = items.filter((item) =>
-    item.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+const searchFilteredItems = ref([])
+const searchFilteredCurrentPage = ref(1)
+const itemsPerPage = 5
+
+function search() {
+  searchFilteredItems.value = accounts.value.filter((account) =>
+    account.userName.toLowerCase().includes(searchQuery.value.toLowerCase())
   )
 }
+const paginatedItems = computed(() => {
+  const start = (searchFilteredCurrentPage.value - 1) * itemsPerPage
+  const end = start + itemsPerPage
+  return searchFilteredItems.value.slice(start, end)
+})
 </script>
 <template>
   <div>
     <div class="search-form">
-      <input v-model="searchQuery" type="text" placeholder="search" />
-      <button @click="search">検索</button>
-      <ul>
-        <li v-for="item in filteredItems" :key="item.id">{{ item.name }}</li>
-      </ul>
+      <b-input-group>
+        <b-form-input v-model="searchQuery" placeholder="search" class="no-outline"></b-form-input>
+        <b-input-group-append>
+          <b-button variant="primary" @click="search">検索</b-button>
+        </b-input-group-append>
+      </b-input-group>
+      <div class="search-result">
+        <b-list-group class="mt-3">
+          <b-list-group-item v-for="(item, index) in paginatedItems" :key="index">
+            {{ item.userName }}
+          </b-list-group-item>
+        </b-list-group>
+        <b-pagination
+          v-model="searchFilteredCurrentPage"
+          :total-rows="searchFilteredItems.length"
+          :per-page="itemsPerPage"
+          class="my-3"
+        />
+      </div>
     </div>
     <TrendTopics class="trend-topics"></TrendTopics>
     <RecommendedUsers class="recommended-users"></RecommendedUsers>
   </div>
 </template>
 <style scoped>
-.search-form {
-}
-.search-form button {
-  margin-left: 10px;
-  position: relative;
-  top: -7px;
+.no-outline:focus {
+  box-shadow: none;
 }
 .trend-topics {
   background-color: rgb(230, 230, 230);
