c98ab3ac4293519ec27aaeb343c754926e443b8b
skajihara <t15cs015@gmail.com>
2024-04-26 15:23:00 +0900

    ブラウザのサイズ変動によるコンテンツの重複を修正

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,28 @@ import RightArea from './components/RightArea.vue'
 </script>
 <template>
   <div id="app">
-    <a href="/home">
-      <img
-        alt="tlogo"
-        class="t-logo"
-        src="@/assets/icons/logo/t-logo.svg"
-        width="100"
-        height="100"
-      />
-    </a>
     <b-container fluid>
       <b-row>
-        <b-col md="3">
-          <LeftArea class="left-area"></LeftArea>
-        </b-col>
-        <b-col md="6">
-          <CenterArea class="center-area"></CenterArea>
-        </b-col>
-        <b-col md="3">
-          <RightArea class="right-area"></RightArea>
-        </b-col>
+        <div class="whole d-flex justify-content-center">
+          <div class="left">
+            <b-col>
+              <a href="/home">
+                <img class="t-logo" src="@/assets/icons/logo/t-logo.svg" width="100" height="100" />
+              </a>
+              <LeftArea class="left-area"></LeftArea>
+            </b-col>
+          </div>
+          <div class="center">
+            <b-col>
+              <CenterArea class="center-area"></CenterArea>
+            </b-col>
+          </div>
+          <div class="right">
+            <b-col>
+              <RightArea class="right-area"></RightArea>
+            </b-col>
+          </div>
+        </div>
       </b-row>
     </b-container>
   </div>
@@ -35,5 +37,18 @@ import RightArea from './components/RightArea.vue'
 }
 #app::-webkit-scrollbar {
   display: none;
+}
+.whole {
+  display: flex;
+  align-content: center;
+}
+.left {
+  min-width: 300px;
+}
+.center {
+  min-width: 700px;
+}
+.right {
+  min-width: 300px;
 }
 </style>
