5f3735621985fd8e404ddb729949273d1dd5fc41
skajihara <t15cs015@gmail.com>
2024-05-07 19:27:54 +0900

    プロジェクトのルートをspringbootに変更 vue関係のファイルを１つのフォルダに集約

---
+++ src/main/java/skajihara/projectX/ProjectXApplication.java
@@ -0,0 +1,13 @@
+package skajihara.projectX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skajihara/projectX/ProjectXApplicationTests.java
@@ -0,0 +1,13 @@
+package skajihara.projectX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
