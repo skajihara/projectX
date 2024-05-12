dcb196757ba6bdfb5b47996efc968656230315e9
skajihara <t15cs015@gmail.com>
2024-05-13 03:17:17 +0900

    ツイートリポジトリクラスの処理を定義

--- src/main/java/skajihara/projectX/MainContents/Home/repository/TweetRepository.java
+++ src/main/java/skajihara/projectX/MainContents/Home/repository/TweetRepository.java
@@ -1,4 +1,32 @@
 package skajihara.projectX.MainContents.Home.repository;
 
-public class TweetRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import skajihara.projectX.MainContents.Home.entity.Tweet;
+
+import java.util.List;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet,Integer> {
+
+    @Query("SELECT t FROM Tweet t WHERE t.deleteFlag = false ORDER BY t.datetime DESC")
+    List<Tweet> selectAll();
+
+    @Query("SELECT t FROM Tweet t WHERE t.deleteFlag = false ORDER BY t.datetime DESC LIMIT :num")
+    List<Tweet> selectRecentN(@Param("num") int num);
 }
+//
+//@Mapper
+//public interface IssueRepository {
+//
+//    @Select("select * from issues")
+//    List<IssueEntity> findAll();
+//
+//    @Insert("insert into issues (summary, description) values (#{summary}, #{description})")
+//    void insert(String summary, String description);
+//
+//    @Select("select * from issues where id = #{issueId}")
+//    IssueEntity findById(long issueId);
+//}
