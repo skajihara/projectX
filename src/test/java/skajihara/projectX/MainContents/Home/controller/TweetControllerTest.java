d5a16cef490be3c707af91164d7fb75184015833
skajihara <t15cs015@gmail.com>
2024-05-16 13:40:37 +0900

    不要なインポートを削除

--- src/test/java/skajihara/projectX/MainContents/Home/controller/TweetControllerTest.java
+++ src/test/java/skajihara/projectX/MainContents/Home/controller/TweetControllerTest.java
@@ -1,16 +1,12 @@
 package skajihara.projectX.MainContents.Home.controller;
 
-//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import skajihara.projectX.MainContents.Home.service.TweetService;
 
 import java.util.ArrayList;
