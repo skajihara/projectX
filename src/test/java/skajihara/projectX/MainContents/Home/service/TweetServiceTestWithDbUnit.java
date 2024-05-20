7772878926acb72eac293bdce1b9aec58558dd77
Yuki Yahata <jpz40243@linecorp.com>
2024-05-20 17:11:04 +0900

    added test code.

---
+++ src/test/java/skajihara/projectX/MainContents/Home/service/TweetServiceTestWithDbUnit.java
@@ -0,0 +1,29 @@
+package skajihara.projectX.MainContents.Home.service;
+
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import skajihara.projectX.MainContents.Home.entity.Tweet;
+import skajihara.projectX.util.CsvDataSetLoader;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+public class TweetServiceTestWithDbUnit {
+    @Autowired private TweetService tweetService;
+
+    @Test
+    public void selectAllTweetTest() {
+        List<Tweet> tweetList = tweetService.selectAllTweets();
+        assertThat(tweetList).hasSize(62);
+    }
+}
