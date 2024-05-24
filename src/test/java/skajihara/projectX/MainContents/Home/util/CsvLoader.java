4af0f1371e0cfd8d8e773d67b8d053441d30d5af
skajihara <t15cs015@gmail.com>
2024-05-24 13:57:45 +0900

    csvloaderコメント削除

--- src/test/java/skajihara/projectX/MainContents/Home/util/CsvLoader.java
+++ src/test/java/skajihara/projectX/MainContents/Home/util/CsvLoader.java
@@ -2,9 +2,7 @@ package skajihara.projectX.MainContents.Home.util;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
-//import skajihara.projectX.MainContents.Home.entity.Account;
 import skajihara.projectX.MainContents.Home.entity.Tweet;
-//import skajihara.projectX.MainContents.Home.repository.AccountRepository;
 import skajihara.projectX.MainContents.Home.repository.TweetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,36 +11,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
 @Component
 public class CsvLoader {
 
-//    @Autowired
-//    private AccountRepository accountRepository;
-
     @Autowired
     private TweetRepository tweetRepository;
-
-//    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-//    public void loadAccounts(String filePath) throws IOException, CsvException {
-//        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
-//            List<String[]> lines = reader.readAll();
-//            for (String[] line : lines) {
-//                Account account = new Account();
-//                account.setId(Long.parseLong(line[0]));
-//                account.setUsername(line[1]);
-//                account.setEmail(line[2]);
-//                account.setPassword(line[3]);
-//                accountRepository.save(account);
-//            }
-//        }
-//    }
 
     public void loadTweets(String filePath){
         try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
