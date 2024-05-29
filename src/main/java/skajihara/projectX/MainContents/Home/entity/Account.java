0afaefb6e6f56489535708c98ccc78b28460980c
skajihara <t15cs015@gmail.com>
2024-05-29 17:41:58 +0900

    エンティティ定義 リポジトリにカスタムメソッド追加

--- src/main/java/skajihara/projectX/MainContents/Home/entity/Account.java
+++ src/main/java/skajihara/projectX/MainContents/Home/entity/Account.java
@@ -1,4 +1,28 @@
 package skajihara.projectX.MainContents.Home.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ACCOUNTS")
 public class Account {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String id;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @Column(length = 200)
+    private String bio;
+
+    @Column(length = 100)
+    private String icon;
+
+    @Column(nullable = false)
+    private boolean validFlag = false;
+
+    @Column(nullable = false)
+    private boolean deleteFlag = false;
 }
--- src/main/java/skajihara/projectX/MainContents/Home/repository/AccountRepository.java
+++ src/main/java/skajihara/projectX/MainContents/Home/repository/AccountRepository.java
@@ -1,4 +1,13 @@
 package skajihara.projectX.MainContents.Home.repository;
 
-public class AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import skajihara.projectX.MainContents.Home.entity.Account;
+
+public interface AccountRepository extends JpaRepository<Account, String> {
+
+    @Query("SELECT a FROM Account a WHERE a.id = :id AND a.validFlag = false AND a.deleteFlag = false")
+    Account selectAccount(@Param("id") String id);
 }
